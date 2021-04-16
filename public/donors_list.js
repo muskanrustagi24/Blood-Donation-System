4c60114440f61632afed32fab9f56e668cb46d6a
muskanrustagi24 <muskanrustagi24@gmail.com>
2021-04-16 11:52:26 +0530

    Home-page

--- public/donors_list.js
+++ public/donors_list.js
@@ -1,33 +1,10 @@
-function fetchDonors (done) {
-    $.get('/api/donors', function (data){
-        done (data)
+$( function () {
+    let donorList = $('#donor-list')
+
+    fetchDonors(function (donors){
+        //donorList.empty()
+        for(donor of donors){
+            donorList.append(createDonor(donor))
+        }
     })
-}
-
-function addDonor(name, age, contact, city, blood_group, any_health_issues, done) {
-    $.post('/api/donors', {
-        name: name,
-        age : age,
-        contact: contact,
-        city: city,
-        blood_group: blood_group,
-        any_health_issues: any_health_issues
-    },  function (data){
-        done (data)
-    })
-
-}
-
-function createDonor () {
-    return $ (`
-        <tr>
-            <td>${donor.name}</td>
-            <td>${donor.age}</td>
-            <td>${donor.contact}</td>
-            <td>${donor.city}</td>
-            <td>${donor.blood_group}</td>
-            <td>${donor.any_health_issues}</td>
-        </tr>
-    `
-    )
-}
+})
